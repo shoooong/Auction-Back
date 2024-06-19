94f50087241712e4f343f6a3f6f4a2a8f6bdc2b1
Sophie-Ahn <sgdream95@gmail.com>
2024-06-19 16:17:18 +0900

    User entity sample

--- backend/src/main/java/com/example/backend/entity/User.java
+++ backend/src/main/java/com/example/backend/entity/User.java
@@ -1,4 +1,28 @@
 package com.example.backend.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
+    @Id
+    private Long userId;
+
+    private String email;
+    private String password;
+    private Long grade;
+    private String nickname;
+    private String phone;
+    private String profileImg;
+    private String Role;
+
+    private LocalDate createDate;
 }
