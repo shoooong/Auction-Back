3fc558550e7c80e4531bfca3751d33a5d597bafd
KSH3535 <157556983+KSH3535@users.noreply.github.com>
2024-07-07 21:25:16 +0900

    Back / Address 엔티티 수정

--- backend/src/main/java/com/example/backend/entity/Address.java
+++ backend/src/main/java/com/example/backend/entity/Address.java
@@ -1,27 +1,46 @@
 package com.example.backend.entity;
 
-import com.example.backend.dto.mypage.addressSettings.AddressDTO;
+import com.example.backend.dto.mypage.addressSettings.AddressReqDto;
 import jakarta.persistence.*;
 import lombok.*;
 
+@Entity
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @ToString
-@Entity
 public class Address {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long addressId;
 
-    @Column(length = 255, nullable = false)
-    private String zoneNo;
+    // 우편 번호
+    @Column(length = 20, nullable = false)
+    private String zoneCode;
 
+    // 도로명 주소
     @Column(length = 255, nullable = false)
+    private String roadAddress;
+
+    // 지번 주소
+    @Column(length = 255, nullable = false)
+    private String jibunAddress;
+
+    // 상세 주소
+    @Column(length = 255, nullable = false)
+    private String detailAddress;
+
+    // 추가 주소 정보
+    @Column(length = 20, nullable = false)
+    private String extraAddress;
+
+    // 배송지명
+    @Column(length = 20, nullable = false)
     private String addressName;
 
+    // 기본 배송지 설정 여부
     @Column(nullable = false)
     private Boolean defaultAddress;
 
@@ -30,9 +49,13 @@ public class Address {
     private Users user;
 
 
-    public void updateAddress(AddressDTO addressDTO) {
-        this.zoneNo = addressDTO.getZoneNo();
-        this.addressName = addressDTO.getAddressName();
-        this.defaultAddress = addressDTO.isDefaultAddress();
+    public void updateAddress(AddressReqDto addressReqDto) {
+        this.zoneCode = addressReqDto.getZoneCode();
+        this.roadAddress = addressReqDto.getRoadAddress();
+        this.jibunAddress = addressReqDto.getJibunAddress();
+        this.detailAddress = addressReqDto.getDetailAddress();
+        this.extraAddress = addressReqDto.getExtraAddress();
+        this.addressName = addressReqDto.getAddressName();
+        this.defaultAddress = addressReqDto.isDefaultAddress();
     }
 }
