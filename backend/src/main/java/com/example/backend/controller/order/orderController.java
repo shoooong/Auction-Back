456be3f0e9a2a70e2eb8cc0d88eedae2a13e6d81
jong1uk <106105315+jong1uk@users.noreply.github.com>
2024-07-11 10:38:29 +0900

    Back / orderController 추가

---
+++ backend/src/main/java/com/example/backend/controller/order/orderController.java
@@ -0,0 +1,14 @@
+package com.example.backend.controller.order;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/order")
+@Log4j2
+public class orderController {
+
+}
