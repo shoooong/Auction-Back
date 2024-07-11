52c34894489061b1cf379d0c074124ef9ffd40f2
jong1uk <106105315+jong1uk@users.noreply.github.com>
2024-07-11 10:35:32 +0900

    Back / CouponCreateDto 수정

--- backend/src/main/java/com/example/backend/dto/coupon/CouponCreateDto.java
+++ backend/src/main/java/com/example/backend/dto/coupon/CouponCreateDto.java
@@ -1,6 +1,8 @@
 package com.example.backend.dto.coupon;
 import com.example.backend.entity.Coupon;
 import com.example.backend.entity.enumData.DiscountType;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,6 +24,9 @@ public class CouponCreateDto {
     private Long expDay;
     private DiscountType discountType;
     private Long amount;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String content;
 
     public Coupon toEntity() {
         return Coupon.builder()
@@ -32,6 +37,9 @@ public class CouponCreateDto {
             .expDay(this.expDay)
             .discountType(this.discountType)
             .amount(this.amount)
+            .startDate(this.startDate)
+            .endDate(this.endDate)
+            .content(this.content)
             .build();
     }
 }
