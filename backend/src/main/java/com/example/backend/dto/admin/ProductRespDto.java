ec69c39d2bb9e21cb042d8d2dbfd6cec5bdd79f6
sue-23 <85052088+sue-23@users.noreply.github.com>
2024-07-15 10:05:08 +0900

    back/상품 대분류 조회

--- backend/src/main/java/com/example/backend/dto/admin/ProductRespDto.java
+++ backend/src/main/java/com/example/backend/dto/admin/ProductRespDto.java
@@ -8,26 +8,21 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ProductRespDto {
-    private Long productId;
+
     private String brand;
     private String productName;
     private String modelNum;
     private String productImg;
     private String mainDepartment;
-    private String subDepartment;
-    private String productSize;
 
     private Long 최저가;
 
-    public ProductRespDto(Long productId, String brand, String productName, String modelNum, String productImg, String mainDepartment, String subDepartment,String productSize, Long 최저가) {
-        this.productId = productId;
+    public ProductRespDto( String brand, String productName, String modelNum, String productImg, String mainDepartment, Long 최저가) {
         this.brand = brand;
         this.productName = productName;
         this.modelNum = modelNum;
         this.productImg = productImg;
         this.mainDepartment = mainDepartment;
-        this.subDepartment = subDepartment;
-        this.productSize = productSize;
         this.최저가 = 최저가;
     }
 }
