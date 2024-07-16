4f86fbe3b732a1eb5405a22f2a567b29d65c2038
KSH3535 <157556983+KSH3535@users.noreply.github.com>
2024-07-16 18:45:27 +0900

    Back / fix : UserController에서 jwtUtil 주입받아 사용하도록 수정

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.example.backend.controller;
 
 import com.example.backend.exception.CustomJWTException;
-import com.example.backend.security.JWTUtil;
 import com.example.backend.dto.user.UserDTO;
 import com.example.backend.dto.user.UserRegisterDTO;
+import com.example.backend.security.JWTUtil;
+import jakarta.servlet.http.Cookie;
 import com.example.backend.service.user.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,7 @@ import java.util.Map;
 public class UserController {
 
     private final UserService userService;
+    private final JWTUtil jwtUtil;
 
     // 로그인
     @PostMapping("/login")
@@ -45,8 +48,8 @@ public class UserController {
 
         Map<String, Object> claims = userDTO.getClaims();
 
-        String kakaoAccessToken = JWTUtil.generateToken(claims, 10);         // 10분
-        String kakaoRefreshToken = JWTUtil.generateToken(claims, 60*24);     // 1일
+        String kakaoAccessToken = jwtUtil.generateToken(claims, 10);         // 10분
+        String kakaoRefreshToken = jwtUtil.generateToken(claims, 60*24);     // 1일
 
         log.info("kakaoAccessToken={}", kakaoAccessToken);
         log.info("kakaoRefreshToken={}", kakaoRefreshToken);
@@ -81,7 +84,8 @@ public class UserController {
     // refreshToken 재발행
     // TODO: 시간별 재발행 여부 test
     @PostMapping("/refresh")
-    public Map<String, Object> refresh(@RequestHeader("Authorization") String authHeader, String refreshToken) {
+    public Map<String, Object> refresh(@RequestHeader("Authorization") String authHeader,
+                                       @CookieValue(value = "refreshToken", required = false) String refreshToken, HttpServletResponse response) {
 
         // 1. accessToken이 없거나 잘못된 JWT인 경우, 예외 메시지 발생
         if (refreshToken == null) {
@@ -96,23 +100,43 @@ public class UserController {
         log.info("Received access token: " + accessToken);
 
         // 2. accessToken의 유효기간 남은 경우, 전달된 토큰들을 그대로 전송
-        if (!JWTUtil.checkExpiredToken(accessToken)) {
+        if (!jwtUtil.checkExpiredToken(accessToken)) {
             log.info("Access token is still valid.");
             return Map.of("accessToken", accessToken, "refreshToken", refreshToken);
         }
 
         // refreshToken 검증
-        Map<String, Object> claims = JWTUtil.validateToken(refreshToken);
+        Map<String, Object> claims = jwtUtil.validateToken(refreshToken);
 
         log.info("refresh ... claims: " + claims );
 
         // 3. accessToken이 만료, refreshToken은 만료되지 않은 경우, 새로운 accessToken만 생성하여 전송
-        String newAccessToken = JWTUtil.generateToken(claims, 10);
+        String newAccessToken = jwtUtil.generateToken(claims, 30);
 
-        String newRefreshToken = JWTUtil.checkTime((Integer)claims.get("exp")) == true ?
-                JWTUtil.generateToken(claims, 60 * 24) : refreshToken;
+        String newRefreshToken = jwtUtil.checkTime((Integer) claims.get("exp")) ?
+                jwtUtil.generateToken(claims, 60 * 24) : refreshToken;
 
-        return Map.of("accessToken", newAccessToken, "refreshToken", newRefreshToken);
+
+        // HttpOnly 쿠키 설정 (새로운 AccessToken)
+//        Cookie newAccessTokenCookie = new Cookie("accessToken", newAccessToken);
+//        newAccessTokenCookie.setHttpOnly(true);
+//        newAccessTokenCookie.setSecure(true);
+//        newAccessTokenCookie.setPath("/");
+//        newAccessTokenCookie.setMaxAge(30 * 60); // 30분
+//        response.addCookie(newAccessTokenCookie);
+
+        // HttpOnly 쿠키 설정 (새로운 RefreshToken이 생성된 경우에만 설정)
+//        if (!newRefreshToken.equals(refreshToken)) {
+//            Cookie newRefreshTokenCookie = new Cookie("refreshToken", newRefreshToken);
+//            newRefreshTokenCookie.setHttpOnly(true);
+//            newRefreshTokenCookie.setSecure(true);
+//            newRefreshTokenCookie.setPath("/");
+//            newRefreshTokenCookie.setMaxAge(60 * 60 * 24); // 1일
+//            response.addCookie(newRefreshTokenCookie);
+//        }
+
+
+        return Map.of("newAccessToken", newAccessToken, "newRefreshToken", newRefreshToken);
     }
 
 }
