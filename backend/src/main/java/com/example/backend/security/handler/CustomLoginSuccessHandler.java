b90405abde7729ada9a18dd2905f6fc7f91ce4d4
KSH3535 <157556983+KSH3535@users.noreply.github.com>
2024-07-19 11:03:25 +0900

    fix : 쿠키 도메인 테스트

--- backend/src/main/java/com/example/backend/security/handler/CustomLoginSuccessHandler.java
+++ backend/src/main/java/com/example/backend/security/handler/CustomLoginSuccessHandler.java
@@ -56,45 +56,45 @@ public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
 
 
         // 쿠키 설정
-//        String domain = "www.sho0ong.com";
-        String domain = request.getServerName();
+        String domain = "sho0ong.com";
+//        String domain = request.getServerName();
 
-        String accessTokenCookie = String.format("accessToken=%s; Max-Age=%d; Path=/; HttpOnly; Secure=false; Domain=%s; SameSite=Lax",
-                accessToken, 120, domain);
-        String refreshTokenCookie = String.format("refreshToken=%s; Max-Age=%d; Path=/; HttpOnly; Secure=false; Domain=%s; SameSite=Lax",
-                refreshToken, 60 * 60 * 24, domain);
-        String isLoginCookie = String.format("isLogin=%s; Max-Age=%d; Path=/; Secure=false; Domain=%s; SameSite=Lax",
-                accessToken, 120, domain);
+//        String accessTokenCookie = String.format("accessToken=%s; Max-Age=%d; Path=/; HttpOnly; Secure=false; Domain=%s; SameSite=Lax",
+//                accessToken, 120, domain);
+//        String refreshTokenCookie = String.format("refreshToken=%s; Max-Age=%d; Path=/; HttpOnly; Secure=false; Domain=%s; SameSite=Lax",
+//                refreshToken, 60 * 60 * 24, domain);
+//        String isLoginCookie = String.format("isLogin=%s; Max-Age=%d; Path=/; Secure=false; Domain=%s; SameSite=Lax",
+//                accessToken, 120, domain);
 
-        response.addHeader("Set-Cookie", accessTokenCookie);
-        response.addHeader("Set-Cookie", refreshTokenCookie);
-        response.addHeader("Set-Cookie", isLoginCookie);
+//        response.addHeader("Set-Cookie", accessTokenCookie);
+//        response.addHeader("Set-Cookie", refreshTokenCookie);
+//        response.addHeader("Set-Cookie", isLoginCookie);
 
-//        Cookie accessTokenCookie = new Cookie("accessToken", accessToken);
-//        accessTokenCookie.setHttpOnly(true);
-//        accessTokenCookie.setSecure(false);
-//        accessTokenCookie.setPath("/");
-//        accessTokenCookie.setMaxAge(60 * 2);
-//        accessTokenCookie.setDomain(domain);
-//
-//        Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
-//        refreshTokenCookie.setHttpOnly(true);
-//        refreshTokenCookie.setSecure(false);
-//        refreshTokenCookie.setPath("/");
-//        refreshTokenCookie.setMaxAge(60 * 60 * 24);
-//        refreshTokenCookie.setDomain(domain);
-//
-//        Cookie isLoginCookie = new Cookie("isLogin", "true");
-//        isLoginCookie.setHttpOnly(false);
-//        isLoginCookie.setSecure(false);
-//        isLoginCookie.setPath("/");
-//        isLoginCookie.setMaxAge(60 * 2);
-//        isLoginCookie.setDomain(domain);
-//
-////         쿠키를 응답에 추가
-//        response.addCookie(accessTokenCookie);
-//        response.addCookie(refreshTokenCookie);
-//        response.addCookie(isLoginCookie);
+        Cookie accessTokenCookie = new Cookie("accessToken", accessToken);
+        accessTokenCookie.setHttpOnly(true);
+        accessTokenCookie.setSecure(false);
+        accessTokenCookie.setPath("/");
+        accessTokenCookie.setMaxAge(60 * 2);
+        accessTokenCookie.setDomain(domain);
+
+        Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
+        refreshTokenCookie.setHttpOnly(true);
+        refreshTokenCookie.setSecure(false);
+        refreshTokenCookie.setPath("/");
+        refreshTokenCookie.setMaxAge(60 * 60 * 24);
+        refreshTokenCookie.setDomain(domain);
+
+        Cookie isLoginCookie = new Cookie("isLogin", "true");
+        isLoginCookie.setHttpOnly(false);
+        isLoginCookie.setSecure(false);
+        isLoginCookie.setPath("/");
+        isLoginCookie.setMaxAge(60 * 2);
+        isLoginCookie.setDomain(domain);
+
+        // 쿠키를 응답에 추가
+        response.addCookie(accessTokenCookie);
+        response.addCookie(refreshTokenCookie);
+        response.addCookie(isLoginCookie);
 
 
         Gson gson = new Gson();
