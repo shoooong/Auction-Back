e7dac1de492ba8402521c3cf7ddf16bdcf595924
jong1uk <106105315+jong1uk@users.noreply.github.com>
2024-07-21 22:59:29 +0900

    BackEnd / DiscountType 수정

--- backend/src/main/java/com/example/backend/entity/enumData/DiscountType.java
+++ backend/src/main/java/com/example/backend/entity/enumData/DiscountType.java
@@ -1,5 +1,18 @@
 package com.example.backend.entity.enumData;
 
 public enum DiscountType {
-    PERCENT, FIXED
+    PERCENT("%"),
+    FIXED("원");
+
+    private String symbol;
+
+    DiscountType(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol(){
+        return symbol;
+    }
+
+
 }
