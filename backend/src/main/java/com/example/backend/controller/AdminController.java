65cf8d68ef4e00a14a8407435bf6c999e7a12ad5
sue-23 <85052088+sue-23@users.noreply.github.com>
2024-07-24 15:59:36 +0900

    back/adminController 엔드로인트 수정

--- backend/src/main/java/com/example/backend/controller/AdminController.java
+++ backend/src/main/java/com/example/backend/controller/AdminController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 
-@RequestMapping("/admin")
+@RequestMapping("/api/admin")
 @RequiredArgsConstructor
 @Log4j2
 @RestController
